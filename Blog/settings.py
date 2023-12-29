85f435df7c7a3af9c169551522289563723712ee
Sujeet <sujeetsmpatel@gmail.com>
2023-12-29 12:06:31 +0530

    modified

--- Blog/settings.py
+++ Blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b!dk#m2wnxh900d$-+krz3v6bfntt!*xv5p)-%&6o+s4+#-r8&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
