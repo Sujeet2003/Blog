f1aa3a78673b58b5a3dbaea14dac8954b274901f
Sujeet <sujeetsmpatel@gmail.com>
2024-01-25 17:16:50 +0530

    Settings for Vercel

--- Blog/settings.py
+++ Blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b!dk#m2wnxh900d$-+krz3v6bfntt!*xv5p)-%&6o+s4+#-r8&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh' , '*']
 
 
 # Application definition
@@ -77,11 +78,18 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'postgres://default:rNFp6Ri8ZIlb@ep-white-term-35420257-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb')
+    )
 }
 
 
@@ -123,7 +131,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR/ 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR/ 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR/ 'build_files', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
